4343afd858f0700311128638d71828cae067d12c
Francisco Contijoch <francisco.contijoch@gmail.com>
2016-02-11 21:20:53 -0800

    Add Dump Frequency 1

--- toolboxes/solvers/gpu/hoCuNlcgSolver.h
+++ toolboxes/solvers/gpu/hoCuNlcgSolver.h
@@ -10,35 +10,47 @@
 namespace Gadgetron{
 
 template<class T> class hoCuNlcgSolver: public nlcgSolver<hoCuNDArray<T> >{
-	typedef typename realType<T>::Type REAL;
+    typedef typename realType<T>::Type REAL;
 public:
-	hoCuNlcgSolver():nlcgSolver<hoCuNDArray<T> >(){
+    hoCuNlcgSolver():nlcgSolver<hoCuNDArray<T> >(){
 
-	}
+    }
 
-	virtual ~hoCuNlcgSolver(){};
+    virtual ~hoCuNlcgSolver(){};
 
-  virtual void iteration_callback(hoCuNDArray<T>* x,int i,REAL data_res,REAL reg_res){
+    virtual void iteration_callback(hoCuNDArray<T>* x,int i,REAL data_res,REAL reg_res){
 
         /*
-	  if (i == 0){
-		  std::ofstream textFile("residual.txt",std::ios::trunc);
-	  	  textFile << data_res << std::endl;
-	  } else{
-		  std::ofstream textFile("residual.txt",std::ios::app);
-		  textFile << data_res << std::endl;
-	  }
-	  std::stringstream ss;
-	  ss << "iteration-" << i << ".real";
+      if (i == 0){
+          std::ofstream textFile("residual.txt",std::ios::trunc);
+          textFile << data_res << std::endl;
+      } else{
+          std::ofstream textFile("residual.txt",std::ios::app);
+          textFile << data_res << std::endl;
+      }
+      std::stringstream ss;
+      ss << "iteration-" << i << ".real";
       write_nd_array(x,ss.str().c_str());
       */
 
-      //if( (counter_ % dumpFreq_) == 0 ){
+        if( (i % dumpFreq_) == 0 )
+        {
         printf("Dumping frame\n");
         char filename[19];
         sprintf(filename, "img-itr-%04i.real", i);
         write_nd_array<float>(x, filename);
-      //}
-  };
+        }
+    };
+
+    void set_dump_frequency(unsigned int dumpFreq) {
+      if( dumpFreq == 0 )
+        this->dumpFreq_ = 9999999; // Not sure how modulus 0 behaves, so just make it a large number that is never reached...
+      else
+        this->dumpFreq_ = dumpFreq;
+    };
+
+protected:
+  unsigned int dumpFreq_;
+
 };
 }
