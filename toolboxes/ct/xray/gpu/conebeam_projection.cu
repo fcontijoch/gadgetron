08263c4ee19160c9e2226cf6b985d4eedc51ddcb
Francisco Contijoch <fcontijoch@franciscos-mbp.dynamic.ucsd.edu>
2016-05-02 16:00:38 -0700

    FC added option for quarter detector offset V9

--- toolboxes/ct/xray/gpu/conebeam_projection.cu
+++ toolboxes/ct/xray/gpu/conebeam_projection.cu
@@ -715,7 +715,7 @@ conebeam_forwards_projection_kernel_cyl( float * __restrict__ projections,
         // Projection plate indices
         //
 
-        const floatd2 ps_pc = floatd2(co[0], co[1]);
+        const floatd2 ps_pc;
         floatd2 ps_pc_shift;
 #ifdef PS_ORIGIN_CENTERING
         //const floatd2 ps_pc = floatd2(co[0], co[1]) + floatd2(0.5);
@@ -729,7 +729,7 @@ conebeam_forwards_projection_kernel_cyl( float * __restrict__ projections,
         {
             ps_pc_shift = floatd2(0.5f,0.5f);
         }
-        ps_pc += ps_pc_shift;
+        ps_pc = floatd2(co[0], co[1]) + ps_pc_shift;
 #else
         ps_pc = floatd2(co[0], co[1]);
 #endif
