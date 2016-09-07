124b2e2c723ab9f56425fee84d506e3da97b9c26
Francisco Contijoch <fcontijoch@Franciscos-MacBook-Pro.local>
2016-09-07 14:28:38 -0700

    FC changed added ffs_ to parameters

--- toolboxes/ct/xray/gpu/hoCuConebeamProjectionOperator.h
+++ toolboxes/ct/xray/gpu/hoCuConebeamProjectionOperator.h
@@ -165,6 +165,7 @@ namespace Gadgetron{
     float samples_per_pixel_;
     bool use_fbp_;
     bool use_cyl_det_;
+    bool ffs_;
     unsigned int projections_per_batch_;
     bool preprocessed_;
     bool short_scan_;
