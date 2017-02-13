82217a2507bd81002b4e926fd5f419a743a325b2
Francisco Contijoch <fcontijoch@Franciscos-MacBook-Pro.local>
2017-02-13 07:17:37 -0800

    FC added flags

--- toolboxes/ct/xray/gpu/conebeam_projection.cu
+++ toolboxes/ct/xray/gpu/conebeam_projection.cu
@@ -876,6 +876,7 @@ conebeam_forwards_projection_cyl(hoCuNDArray<float> *projections,
                                   float SAD,
                                   bool ffs)
 {
+    printf("conbeam_projection.cu - Start BackProject_Cyl Iter \n");
     //
     // Validate the input
     //
@@ -1978,7 +1979,7 @@ void conebeam_backwards_projection_cyl( hoCuNDArray<float> *projections,
                                         cuNDArray<float> *frequency_filter
                                         )
 {
-    // printf("conbeam_projection.cu - Start BackProject_Cyl Kernel \n");
+    printf("conbeam_projection.cu - Start BackProject_Cyl FBP \n");
 
     printf("Input Parameters....\n");
     printf("Is_Dims_In_Pixels: X = %d , Y=%d, Z = %d\n",is_dims_in_pixels[0],is_dims_in_pixels[1],is_dims_in_pixels[2]);
